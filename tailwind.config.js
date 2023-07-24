a810ef233944b72f6291b2beb1cf408454eab0fe
esthermadu <prityexsta@gmail.com>
2023-07-24 09:32:26 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "green-500": "#10A866",
+        "yellow-500": "#FFCC05",
+        gold: "#FFD700",
+      },
+      fontFamily: {
+        sans: ['"DM Sans"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
